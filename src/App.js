7b666582b051c0bdb1b317bd51df5f05d29f61ef
Jeannette <jeannetteshui.ng@gmail.com>
2019-01-29 13:11:11 -0500

    react template with react joyride

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Joyride from "react-joyride";
 
 class App extends Component {
+  state = {
+    steps: [
+      {
+        target: ".my-first-step",
+        content: "This is super awesome feature "
+      },
+      {
+        target: ".my-other-step",
+        content: "Everyone's learning Joyride!"
+      }
+    ]
+  };
+
   render() {
+    const { steps } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        <Joyride steps={steps} />
+        <header className="App-header ">
+          <a href="https://reactjs.org" target="_blank">
+            <img src={logo} className="App-logo my-first-step" alt="logo" />
+          </a>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            className="App-link my-other-step"
+            href="https://github.com/gilbarbara/react-joyride"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Joyride
           </a>
         </header>
       </div>
